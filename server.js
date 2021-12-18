9330728fb7cb034fc8ab54b3c74f498138111d3f
Sowmiya-Ramesh <miyaramesh9944@gmail.com>
2021-12-18 16:13:18 +0530

    commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on the port ${PORT}`)); 
+
+
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
+
+
+// NODE_ENV = development
+// PORT = 5000
+// MONGO_URI =mongodb+srv://Sowmiya_Ramesh:guvi787@cluster0.mgtws.mongodb.net/hireWorld?retryWrites=true&w=majority
+// JWT_SECRET = abc123
